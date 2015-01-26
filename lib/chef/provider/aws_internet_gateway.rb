143f9a6d17aa331427de77f41bb21971c8a97005
Erik van Brakel <erik@evision-software.com>
2015-01-26 13:32:28 +0100

    Implement internet gateway support

--- lib/chef/provider/aws_internet_gateway.rb
+++ lib/chef/provider/aws_internet_gateway.rb
@@ -6,14 +6,14 @@ class Chef::Provider::AwsInternetGateway < Chef::Provider::AwsProvider
     converge_by "Creating new internet gateway" do
       gateway = ec2.internet_gateways.create()
       gateway.tags['Name'] = new_resource.name
+
+      existing_vpc = ec2.vpcs.with_tag('Name', new_resource.vpc_name).first
+
+      gateway.attach(existing_vpc)
+
+      ec2.route_tables.main_route_table.create_route("0.0.0.0/0", { :internet_gateway => gateway.id })
+
+      new_resource.save
     end
-
-    existing_vpc = ec2.vpcs.with_tag('Name', new_resource.vpc_name).first
-
-    gateway.attach(existing_vpc)
-
-    ec2.route_tables.main_route_table.create_route("0.0.0.0/0", { :internet_gateway => gateway.id })
-
-    new_resource.save
   end
 end
